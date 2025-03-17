1917c833dbf6b81dfb02bf2871ded01fa98bb76f
barroso <joaodb.barroso@gmail.com>
2025-03-17 18:20:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Joao Barroso Portfolio
+
+This is a portfolio project showcasing my work and skills.
+
+## Contact
+
+- **Email:** joao.barroso@example.com
+- **LinkedIn:** [Joao Barroso](https://www.linkedin.com/in/joão-diogo-barroso/)
---
+++ src/components/footer/index.tsx
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+
+const Footer = () => {
+  const links = [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/jo%C3%A3o-diogo-barroso/",
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/JoaoBBarroso",
+    },
+  ];
+
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      {links.map((link, index) => (
+        <Fragment key={link.name}>
+          <Link
+            key={link.name}
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sm underline"
+          >
+            {link.name}
+          </Link>
+          {index < links.length - 1 && (
+            <Separator className="h-5" orientation="vertical" />
+          )}
+        </Fragment>
+      ))}
+    </footer>
+  );
+};
+
+export default Footer;
